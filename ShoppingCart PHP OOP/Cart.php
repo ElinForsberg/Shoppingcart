f41aca59b1f2b36ebc09ff0affe1702bc46f970a
ElinForsberg <114143740+ElinForsberg@users.noreply.github.com>
2022-12-06 10:27:42 +0100

    Add files via upload

---
+++ ShoppingCart PHP OOP/Cart.php
@@ -0,0 +1,68 @@
+<?php
+
+
+class Cart
+{
+    private array $items = [];
+
+    //TODO Skriv getter för items
+    public function getItems() 
+    {
+        return $this->items;
+    }
+    /*
+     Skall lägga till en produkt i kundvagnen genom att
+     skapa ett nytt cartItem och lägga till i $items array.
+     Metoden skall returnera detta cartItem.
+
+     VG: Om produkten redan finns i kundvagnen
+     skall istället quantity på cartitem ökas.
+     */
+    public function addProduct($product)
+    {
+       $CartItem = $this->items[$product->getId()] ?? null;
+       
+       if($CartItem=== null) {
+        $CartItem = new CartItem($product, 0);
+            $this->items[$product->getId()] = $CartItem;
+       }
+       $CartItem->increaseQuantity();
+       return $CartItem;
+    }
+
+
+    //Skall ta bort en produkt ur kundvagnen (använd unset())
+    public function removeProduct($product)
+    {
+     unset($this->items[$product->getId()]);
+    }
+  
+    //Skall returnera totala antalet produkter i kundvagnen
+    //OBS: Ej antalet unika produkter
+    public function getTotalQuantity()
+    {
+        $calcQty = 0;
+        foreach ($this->items as $item)
+        {
+            $calcQty += $item->getQuantity();
+        }
+        $totQty = 0+$calcQty;
+        return $totQty;
+    }
+
+    //Skall räkna ihop totalsumman för alla produkter i kundvagnen
+    //VG: Tänk på att ett cartitem kan ha olika quantity
+    public function getTotalSum()
+    {
+        $calcPrice =0;
+        foreach ($this->items as $item)
+        {
+            $calcPrice += $item->getProduct()->getPrice() * $item->getQuantity();
+        }
+        $totSum = 0+$calcPrice;
+        return $totSum;
+    }
+
+}
+
+
---
+++ ShoppingCart PHP OOP/Product.php
@@ -0,0 +1,39 @@
+<?php
+
+
+class Product
+{
+    public function __construct($id, $title, $price, $inStock)
+    {
+        $this ->id = $id;
+        $this ->title = $title;
+        $this ->price = $price;
+        $this ->inStock = $inStock;
+    }
+
+    private $id;
+    private $title;
+    private $price;
+    private $inStock;
+
+
+    public function getId(){
+        return $this->id;
+    }
+
+    public function getTitle(){
+        return $this->title;
+    }
+
+    public function getPrice(){
+        return $this->price;
+    }
+
+    public function getinStock(){
+        return $this->inStock;
+    }
+    // TODO Skriv en konstruktor som sätter alla properties
+
+    // TODO Skriv getters för alla properties
+
+}
